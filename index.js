120e4c795c28b4dd6d52a29f7b852b3eb5b93bc4
shuangxunian <shuangxunian@gmail.com>
2022-08-15 20:41:13 +0800

    修改登录方式 用process登录

--- index.js
+++ index.js
@@ -6,13 +6,11 @@ const account = 836473734
 const bot = createClient(account)
 
 bot
-.on("system.login.qrcode", function (e) {
-	this.logger.mark("扫码后按Enter完成登录")
-	process.stdin.once("data", () => {
-		this.login()
-	})
+.on("system.login.slider", function (e) {
+        console.log("输入ticket：")
+        process.stdin.once("data", ticket => this.submitSlider(String(ticket).trim()))
 })
-.login()
+.login(process.env.QQPWD)
 
 exports.bot = bot
 
